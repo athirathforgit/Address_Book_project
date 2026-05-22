dca0119876820cf9fdd862caa1ca251b2979aa44
Sowndharya <sowndharya0807@gmail.com>
2026-05-22 11:17:23 +0530

    Update operations.c

---
+++ src/operations.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "operations.h"
+ 
+void add_contact()
+{
+    printf("Add Contact feature coming soon\n");
+}
+ 
+void search_contact()
+{
+    printf("Search Contact feature coming soon\n");
+}
+ 
+void edit_contact()
+{
+    printf("Edit Contact feature coming soon\n");
+}
+ 
+void delete_contact()
+{
+    printf("Delete Contact feature coming soon\n");
+}
+ 
+void list_contacts()
+{
+    printf("List Contacts feature coming soon\n");
+}
