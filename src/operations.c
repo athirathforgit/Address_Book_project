040ebc30af7914a78029e7c4e77cefc6a7c9eb3d
athirathforgit <athirathforgit927@gmail.com>
2026-05-23 10:11:43 +0000

    completed all fucntions now validation and debugging is there

--- src/operations.c
+++ src/operations.c
@@ -4,6 +4,11 @@
  
 void add_contact(struct Contact contacts[], int * count)
 {
+    if(*count >= MAX_CONTACTS)
+    {
+        printf("Contact List Full\n");
+        return;
+    }
     /* reading name*/
    while(1)
     {
@@ -54,18 +59,164 @@ while(1)
  
 void search_contact(struct Contact contacts[], int count)
 {
-    printf("Search Contact feature coming soon\n");
+    char search_name[NAME_LEN];
+    int found = 0;
+
+    printf("Enter Name to Search: ");
+    scanf(" %[^\n]", search_name);
+
+    for(int i = 0; i < count; i++)
+    {
+        int same = 1;
+
+        for(int j = 0; search_name[j] != '\0' || contacts[i].name[j] != '\0';j++)
+        {
+            if(search_name[j] != contacts[i].name[j])
+            {
+                same = 0;
+                break;
+            }
+        }
+
+        if(same)
+        {
+            printf("\nContact Found\n");
+
+            printf("Name   : %s\n", contacts[i].name);
+            printf("Phone  : %s\n", contacts[i].phone);
+            printf("Email  : %s\n", contacts[i].email);
+
+            found = 1;
+
+            break;
+        }
+    }
+
+    if(found == 0)
+    {
+        printf("\nContact Not Found\n");
+    }
 }
  
 void edit_contact(struct Contact contacts[], int count)
 {
-    printf("Edit Contact feature coming soon\n");
+    char search_name[NAME_LEN];
+    int found = 0;
+
+    printf("Enter Name to Edit: ");
+    scanf(" %[^\n]", search_name);
+
+    for(int i = 0; i < count; i++)
+    {
+        int same = 1;
+
+        for(int j = 0;
+            search_name[j] != '\0' || contacts[i].name[j] != '\0';
+            j++)
+        {
+            if(search_name[j] != contacts[i].name[j])
+            {
+                same = 0;
+                break;
+            }
+        }
+
+        if(same)
+        {
+            found = 1;
+
+            printf("\nEnter New Name: ");
+            scanf(" %[^\n]", contacts[i].name);
+
+            while(!validate_name(contacts[i].name))
+            {
+                printf("Invalid Name\n");
+
+                printf("Enter New Name: ");
+                scanf(" %[^\n]", contacts[i].name);
+            }
+
+            printf("Enter New Phone: ");
+            scanf(" %[^\n]", contacts[i].phone);
+
+            while(!validate_phone(contacts[i].phone))
+            {
+                printf("Invalid Phone Number\n");
+
+                printf("Enter New Phone: ");
+                scanf(" %[^\n]", contacts[i].phone);
+            }
+
+            printf("Enter New Email: ");
+            scanf(" %[^\n]", contacts[i].email);
+
+            while(!validate_email(contacts[i].email))
+            {
+                printf("Invalid Email\n");
+
+                printf("Enter New Email: ");
+                scanf(" %[^\n]", contacts[i].email);
+            }
+
+            printf("\nContact Updated Successfully\n");
+
+            break;
+        }
+    }
+
+    if(found == 0)
+    {
+        printf("\nContact Not Found\n");
+    }
 }
+
  
-void delete_contact(struct Contact contacts[], int * count)
+void delete_contact(struct Contact contacts[], int *count)
 {
-    printf("Delete Contact feature coming soon\n");
+    char search_name[NAME_LEN];
+    int found = 0;
+
+    printf("Enter Name to Delete: ");
+    scanf(" %[^\n]", search_name);
+
+    for(int i = 0; i < *count; i++)
+    {
+        int same = 1;
+
+        for(int j = 0;
+            search_name[j] != '\0' || contacts[i].name[j] != '\0';
+            j++)
+        {
+            if(search_name[j] != contacts[i].name[j])
+            {
+                same = 0;
+                break;
+            }
+        }
+
+        if(same)
+        {
+            found = 1;
+
+            for(int k = i; k < *count - 1; k++)
+            {
+                contacts[k] = contacts[k + 1];
+            }
+
+            (*count)--;
+
+            printf("\nContact Deleted Successfully\n");
+
+            break;
+        }
+    }
+
+    if(found == 0)
+    {
+        printf("\nContact Not Found\n");
+    }
 }
+
  
 void list_contacts(struct Contact contacts[], int count)
 {
